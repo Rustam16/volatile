e6800b3f59f92a277a8b1a954723a7d3395d6b48
Rustam16 <muhambetovrustam1@gmail.com>
2020-11-29 18:17:24 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+
+        myThread.shutdown();
+    }
+}
+
+class MyThread extends Thread {
+    private volatile  boolean running = true;
+
+    public void run() {
+        while (running) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(700);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+    }
+    public void shutdown() {
+        this.running = false;
+    }
+}
+
